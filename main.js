950ab165a2be936d94d82f2cf9e2cbb767df8275
scudrt <1208044562@qq.com>
2019-11-25 18:59:03 +0800

    third commit

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ var graphWidth = window.innerWidth;
 var graphHeight = window.innerHeight;
 const SQUAREROOT3 = Math.sqrt(3);
 
+//global values
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, graphWidth/graphHeight, 0.1, 1000);
+scene.add(camera);
+var isPerspective = true;
+
+var model = createMyModel();
 
 //set renderer
 var renderer = new THREE.WebGLRenderer();
@@ -13,24 +18,82 @@ document.body.appendChild(renderer.domElement);
 //set position of camera
 camera.position.x = 0;
 camera.position.y = 0;
-camera.position.z = 8;
+camera.position.z = 40;
 
 //return a created object, and you can perform operations on it
-function createMyObject(){
-    var geometry = new THREE.BoxBufferGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add(cube);
-    return cube;
+function createMyModel(){
+    var model = new THREE.Mesh(
+        new THREE.TorusKnotGeometry( 10, 3, 100, 16 ),
+        new THREE.MeshBasicMaterial( { color: 0xaa00ff } )
+    );
+    model.matrixAutoUpdate = false;
+    scene.add(model);
+    return model;
+}
+
+// function called when a key was pressed
+document.onkeypress = function(event){
+    var code = String.fromCharCode(
+        event.keyCode
+    ).toUpperCase();
+    if ('W' == code){
+        ;
+    }else if ('S' == code){
+        ;
+    }else if ('A' == code){
+        ;
+    }else if ('D' == code){
+        ;
+    }else if ('J' == code){
+        scaleModel(0.95);
+    }else if ('L' == code){
+        scaleModel(1.05);
+    }else if ('P' == code){
+        switchProjection();
+    }
+}
+
+var eye = new THREE.Matrix4();
+eye.identity();
+function scaleModel(scaleDelta){
+    var scaleMatrix = new THREE.Matrix4();
+    scaleMatrix.set(
+        scaleDelta, 0, 0, 0,
+        0, scaleDelta, 0, 0,
+        0, 0, scaleDelta, 0,
+        0, 0, 0, 1
+    );
+    eye.premultiply(scaleMatrix);
+}
+
+function changeViewport(VPMatrix){
+}
+
+function lookat(){
+    // change camera
+}
+
+function switchProjection(){
+    isPerspective = !isPerspective;
+    var cameraPosition = camera.position.clone();
+    scene.remove(camera);
+    if (isPerspective){
+        camera = new THREE.PerspectiveCamera(75, graphWidth/graphHeight, 0.1, 1000);
+    }else{
+        camera = new THREE.OrthographicCamera(
+            graphWidth / -2, graphWidth / 2,
+            graphHeight / -2, graphHeight / 2,
+            0.1, 1000
+        );
+    }
+    console.log(cameraPosition);
+    camera.position.copy(cameraPosition);
 }
 
 //render function
 var render = function(){
     requestAnimationFrame(render);
-    cube.rotation.x += 0.05;
-    cube.rotation.y += 0.05;
+    model.matrix.copy(eye);
     renderer.render(scene, camera);
 }
-
-var cube = createMyObject();
 render();
